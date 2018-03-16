baa53fe9ba18dea8a3c4846483b79cb231369f92
Colby Taperts <colbytaperts@gmail.com>
2018-03-15 23:37:47 -0500

    start creating side drawer for mobile app

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,6 +98,7 @@ class BurgerBuilder extends Component {
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary
+            price={this.state.totalPrice}
             ingredients={this.state.ingredients}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinue={this.purchaseContinueHandler}
