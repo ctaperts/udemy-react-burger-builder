696ab16e93d7a2f47f924ab477a78bb4e71d58c5
Colby Taperts <colbytaperts@gmail.com>
2018-03-18 21:04:04 -0500

    setup post request with axios with order info + dummy info

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,8 +84,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('Coninue')
-  };
+    // alert('Continue')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Colby Taperts',
+        address: {
+          street: '1029 laurel st',
+          zipeCode: '12345',
+          country: 'USA'
+        },
+        email: 'ckt@gmail.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  }
 
 
   render (){
