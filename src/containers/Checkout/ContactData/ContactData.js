7d44434af67bf1b16158ea5e40950d4134d657cf
Colby Taperts <colbytaperts@gmail.com>
2018-04-02 23:35:19 -0500

    setup input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,15 +4,63 @@ import Button from '../../../components/UI/Button/Button';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
-    }
+    orderForm: {
+      name: {
+        inputtype: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        inputtype: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipeCode: {
+        inputtype: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code'
+        },
+        value: ''
+      },
+      country: {
+        inputtype: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        inputtype: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your E-mail'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        inputtype: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'}
+          ]
+        },
+        value: ''
+      },
+    },
+    loading: false
   };
 
   orderHandler = (event) => {
@@ -46,10 +94,10 @@ class ContactData extends Component {
   render() {
     let form = (
         <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-          <input className={classes.Input} type="text" name="street" placeholder="Street" />
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
+          <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+          <Input inputtype="input" type="email" name="email" placeholder="Your email" />
+          <Input inputtype="input" type="text" name="street" placeholder="Street" />
+          <Input inputtype="input" type="text" name="postal" placeholder="Postal code" />
           <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
       );
