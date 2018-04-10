22a51594f5df98e76d8e7310f895b867da02da83
Colby Taperts <colbytaperts@gmail.com>
2018-04-09 23:24:13 -0500

    add sign in with firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -20,8 +22,26 @@ export const authFail = (error) => {
   }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBD2iLzEkQx7SFusv9dcnXLb6LX01Z4nz4';
+    if (isSignup) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBD2iLzEkQx7SFusv9dcnXLb6LX01Z4nz4';
+    }
+    axios.post(url, authData)
+      .then(response => {
+        console.log(response)
+        dispatch(authSuccess(authData))
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch(authFail(err))
+      })
   }
 }
