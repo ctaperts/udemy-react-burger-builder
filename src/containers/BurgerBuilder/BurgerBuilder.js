3a16d040a5c76ee675c3c400e3414ee070fb5f03
Colby Taperts <colbytaperts@gmail.com>
2018-04-12 21:03:28 -0500

    moar tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actionCreator from '../../store/actions/index'  // can remove index *
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false
   };
